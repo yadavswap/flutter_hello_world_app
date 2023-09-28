5229a1d9cc7d5b42f947e97fefdf03ccc2853987
Swapnil Yadav <46942236+yadavswap@users.noreply.github.com>
2023-09-28 18:42:23 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  var app = MaterialApp(
+    title: 'Flutter App',
+    debugShowCheckedModeBanner: true,
+    home: Scaffold(appBar: AppBar()),
+  );
+  runApp(app);
+}
