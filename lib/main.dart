29282483d5a19ca67c1687856b91d32b799f2439
Swapnil Yadav <46942236+yadavswap@users.noreply.github.com>
2023-10-02 14:43:06 +0530

    ElevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,38 @@ import 'package:flutter/material.dart';
 void main() {
   var app = MaterialApp(
     title: 'Flutter App',
-    debugShowCheckedModeBanner: true,
-    home: Scaffold(appBar: AppBar()),
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primaryColor: Colors.green,
+      hintColor: Colors.orange,
+    ),
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter'),
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Hello, I\'m here'),
+            const Text('Hello'),
+            ElevatedButton(
+              child: Text('Login'),
+              onPressed: () {},
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.orange),
+                  overlayColor: MaterialStateProperty.all(Colors.blue)),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add_a_photo),
+      ),
+    ),
   );
   runApp(app);
 }
